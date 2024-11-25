889842996ed27f0f9ebba21ccfa25cfd1f6f10de
devxpat <devxpat@seas.upenn.edu>
2024-11-24 21:05:42 -0500

    added routes, categories, numCategories

--- backend/routes.js
+++ backend/routes.js
@@ -57,6 +57,8 @@ const city_distance = async function (req, res) {
 // Route 3: GET /attractions
 const route_attractions = async function (req, res) {
     const categories = req.query.categories;
+    const page = req.query.page
+    const offset = 10 * (page - 1)
    
     connection.query(
     `WITH temp AS (
@@ -74,7 +76,7 @@ const route_attractions = async function (req, res) {
     SELECT *
     FROM temp
     ORDER BY temp.rating desc
-    LIMIT 10;`, (err, data) => {
+    LIMIT 10 OFFSET ${offset};`, (err, data) => {
       if (err) {
           console.log(err);
           res.json({});
@@ -83,12 +85,130 @@ const route_attractions = async function (req, res) {
       }
     });
 }
+
+
+// Route 4: GET /routes/:numInt
+const routes = async function (req, res) {
+  const numInt = req.params.numInt
+  const startCity = req.query.startCity;
+  const startState = req.query.startState;
+  const endCity = req.query.endCity;
+  const endState = req.query.endState;
+  const orderBy = req.query.orderBy ?? '';
+ 
+  if (numInt == 0) {
+    connection.query(
+      `SELECT src.name, src.state, tgt.name, tgt.state, (r1.distance) AS total_distance
+      FROM routes r1
+         JOIN (SELECT id, name, state FROM cities) src ON r1.startCity = src.id
+         JOIN (SELECT id, name, state FROM cities) tgt ON r1.endCity = tgt.id
+      WHERE src.name LIKE '%${startCity}%' AND src.state LIKE '%${startState}%' AND tgt.name LIKE '%${endCity}%' AND tgt.state LIKE '%${endState}%'`, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.json({});
+        } else {
+          res.json(data.rows);
+        }
+      });
+  } else if (numInt == 1) {
+    connection.query(
+      `SELECT src.name, src.state, int1.name, int1.state, tgt.name, tgt.state, (r1.distance + r2.distance) AS total_distance
+      FROM routes r1 JOIN routes r2 ON r1.endCity = r2.startCity
+         JOIN (SELECT id, name, state FROM cities) src ON r1.startCity = src.id
+         JOIN (SELECT id, name, state FROM cities) int1 ON r1.endCity = int1.id
+         JOIN (SELECT id, name, state FROM cities) tgt ON r2.endCity = tgt.id
+      WHERE src.name LIKE '%${startCity}%' AND src.state LIKE '%${startState}%' AND tgt.name LIKE '%${endCity}%' AND tgt.state LIKE '%${endState}%'`, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.json({});
+        } else {
+          res.json(data.rows);
+        }
+      });
+  } else if (numInt == 2) {
+    connection.query(
+      `SELECT src.name, src.state, int1.name, int1.state, int2.name, int2.state, tgt.name, tgt.state, (r1.distance + r2.distance + r3.distance) AS total_distance
+      FROM routes r1 JOIN routes r2 ON r1.endCity = r2.startCity
+         JOIN routes r3 ON r2.endCity = r3.startCity
+         JOIN (SELECT id, name, state FROM cities) src ON r1.startCity = src.id
+         JOIN (SELECT id, name, state FROM cities) int1 ON r1.endCity = int1.id
+         JOIN (SELECT id, name, state FROM cities) int2 ON r2.endCity = int2.id
+         JOIN (SELECT id, name, state FROM cities) tgt ON r3.endCity = tgt.id
+      WHERE src.name LIKE '%${startCity}%' AND src.state LIKE '%${startState}%' AND tgt.name LIKE '%${endCity}%' AND tgt.state LIKE '%${endState}%'`, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.json({});
+        } else {
+          res.json(data.rows);
+        }
+      });
+  } else if (numInt == 3) {
+    connection.query(
+      `SELECT src.name, src.state, int1.name, int1.state, int2.name, int2.state, int3.name, int3.state, tgt.name, tgt.state, (r1.distance + r2.distance + r3.distance + r4.distance) AS total_distance
+      FROM routes r1 JOIN routes r2 ON r1.endcity = r2.startcity
+         JOIN routes r3 ON r2.endcity = r3.startcity
+         JOIN routes r4 ON r3.endcity = r4.startcity
+         JOIN (SELECT id, name, state FROM cities) src ON r1.startCity = src.id
+         JOIN (SELECT id, name, state FROM cities) int1 ON r1.endCity = int1.id
+         JOIN (SELECT id, name, state FROM cities) int2 ON r2.endCity = int2.id
+         JOIN (SELECT id, name, state FROM cities) int3 ON r3.endCity = int3.id
+         JOIN (SELECT id, name, state FROM cities) tgt ON r4.endCity = tgt.id
+      WHERE src.name LIKE '%${startCity}%' AND src.state LIKE '%${startState}%' AND tgt.name LIKE '%${endCity}%' AND tgt.state LIKE '%${endState}%'`, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.json({});
+        } else {
+          res.json(data.rows);
+        }
+      });
+  } else {
+    console.log(err);
+    res.json({});
+  }
+}
    
+// Route 5: GET /subcategories/:cityid
+const subcategories = async function (req, res) {
+  connection.query(
+  `SELECT unnest(string_to_array(a.subcategories, ', ')) AS subcategories
+  FROM attractions a
+  WHERE a.cityid = ${req.params.cityid};`, (err, data) => {
+    if (err) {
+        console.log(err);
+        res.json({});
+    } else {
+      res.json(data.rows[0]);
+    }
+  });
+}
    
+// Route 6: GET /numSubcategories/:cityid
+const numSubcategories = async function (req, res) {
+  connection.query(
+  `SELECT DISTINCT category, COUNT(*) AS attractionCount
+  FROM (
+     SELECT unnest(string_to_array(a.categories, ', ')) AS category
+     FROM attractions a
+     WHERE a.cityid = ${req.params.cityid}
+  ) AS split_categories
+  GROUP BY category;`, (err, data) => {
+    if (err) {
+        console.log(err);
+        res.json({});
+    } else {
+      res.json(data.rows[0]);
+    }
+  });
+}
+
+// hello
    
 
 module.exports = {
     city_info,
     city_distance,
-    route_attractions
+    route_attractions,
+    routes, 
+    subcategories,
+    numSubcategories
 }
