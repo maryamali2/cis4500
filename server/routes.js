48c528fe94d018b0373cb62856da0b668ca7af1d
Kristine <krhuang@seas.upenn.edu>
2024-12-12 17:24:16 -0500

    fixed return value of rankCitiesByUniqueAttractions

--- server/routes.js
+++ server/routes.js
@@ -304,10 +304,7 @@ const rankCitiesByUniqueAttractions = async function(req, res) {
   const cityIdsArray = cityIds.split(',').map(id => parseInt(id, 10)).filter(Boolean);
   if (cityIdsArray.length === 0) {
     return res.json([]);
-  } else if (cityIdsArray.length < 3) {
-    return res.json({"not enough cities for a route"});
-  }
-
+  } 
   connection.query(
     `WITH temp AS (
        SELECT c.id AS CityID, c.name AS CityName, COUNT(DISTINCT a.id) AS NumAttractions
@@ -318,7 +315,7 @@ const rankCitiesByUniqueAttractions = async function(req, res) {
        ORDER BY NumAttractions DESC
        LIMIT 3
      ), temp1 AS (SELECT * FROM TEMP LIMIT 1), temp2 AS (SELECT * FROM TEMP LIMIT 1 OFFSET 1), temp3 AS (SELECT * FROM TEMP LIMIT 1 OFFSET 2)
-      SELECT r1.startcity AS city1, r1.endcity AS city2, r2.endcity AS city3, r1.distance + r2.distance AS distance, temp1.NumAttractions + temp2.NumAttractions + temp3.NumAttractions AS totalNumberOfAttractions
+      SELECT temp1.cityName AS city1, temp2.cityName AS city2, temp3.cityName AS city3, r1.distance + r2.distance AS distance, temp1.NumAttractions + temp2.NumAttractions + temp3.NumAttractions AS totalNumberOfAttractions
     FROM Routes r1 JOIN temp1 ON r1.startcity = temp1.CityID JOIN temp2 ON r1.endcity = temp2.CityID JOIN Routes r2 ON r1.endcity = r2.startcity JOIN temp3 ON r2.endcity = temp3.CityID;
     `,
     [cityIdsArray],
